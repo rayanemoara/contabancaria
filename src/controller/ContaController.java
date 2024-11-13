509081687d6c89a7cb51e611f87896dead988df6
rayane moara <rayane_moara@hotmail.com>
2024-11-13 02:27:10 -0300

    task05

--- src/controller/ContaController.java
+++ src/controller/ContaController.java
@@ -1,41 +1,106 @@
 package controller;
 
+import java.util.ArrayList;
+
 import conta.model.Conta;
+import conta.repository.ContaRepository;
 
-
-public class ContaController {
-    
-    public void procurarPorNumero() {
-    	
-    }
-    
-    public void listarTodas() {
-
-	}
-
-	public void cadastrar(Conta conta) {
-
-    }
-
-	public void atualizar(Conta conta) {
-
-	}
-
-    public void deletar(int numero) {
-    	
-    }
-    
-    public void sacar(int numero, float valor) {
-    	
-    }
-
-	public void depositar(int numero, float valor) {
+public class ContaController implements ContaRepository {
+	
+	private ArrayList<Conta> listaContas = new ArrayList<Conta>();
+	int numero = 0;
+	
+	public void procurarPorNumero(int numero) {
+		var conta = buscarNaCollection(numero);
 		
+		if (conta != null)
+			conta.visualizar();
+		else
+			System.out.println("\nA Conta: " + numero + " não foi encontrada!");
+		
+	}
+
+	public void listarTodas() {
+		for (var conta : listaContas) {
+			conta.visualizar();
+		}
+	}
+
+	public void cadastrar (Conta conta){
+		listaContas.add(conta);
+		System.out.println("\nA Conta número: " + conta.getNumero() + " foi criada com sucesso!");	
+	}
+
+	public void atualizar (Conta conta) {
+		var buscaConta = buscarNaCollection(conta.getNumero());
+		
+		if (buscaConta != null) {
+			listaContas.set(listaContas.indexOf(buscaConta), conta);
+			System.out.println("\nA Conta número: " + conta.getNumero() + " foi atualizada com sucesso!");;
+		} else
+			System.out.println("\nA Conta número: " + conta.getNumero() + " não foi encontrada!");
+		
+	} 
+	
+	public void sacar (int numero, float valor){
+		var conta = buscarNaCollection(numero);
+		
+		if (conta != null) {
+			if (conta.sacar(valor) == true) 
+				System.out.println("\nO Saque da conta número: " + numero + " Foi efetuado com sucesso!");
+		
+		} else 
+			System.out.println("\nA Conta número: " + numero + " não foi encontrada!");
+		}
+
+	
+	public void depositar (int numero, float valor){
+		var conta = buscarNaCollection(numero);
+		
+		if (conta != null) {
+			conta.depositar(valor);
+			System.out.println("\nO Depósito na Conta número: " + numero +  " foi efetuado com sucesso!");
+		} else 
+			System.out.println("\nA Conta número: " + numero + " não foi encontrada!");
+		}
+	
+	public void transferir (int numeroOrigem, int numeroDestino, float valor) {
+		var contaOrigem = buscarNaCollection(numeroOrigem);
+		var contaDestino = buscarNaCollection(numeroDestino);
+		
+		if (contaOrigem != null && contaDestino != null) {
+			if (contaOrigem.sacar(valor) == true) {
+				contaDestino.depositar(valor);
+				System.out.println("\nA Transferência foi efetuada com sucesso!");
+			}
+			
+		} else 
+			System.out.println("\nA Conta de origem e/ou destino não foram encontradas!");
+		} 
+	
+	
+	public void deletar (int numero) {
+		var conta = buscarNaCollection(numero);
+		
+		if (conta != null) {
+			if (listaContas.remove(conta) == true)
+				System.out.println("\nA Conta número: " + numero + " foi deletada com sucesso!");
+		} else 
+			System.out.println("\nA Conta número: " + numero + " não foi encontrada!");
+		}
+	
+	public int gerarNumero() {
+		return ++ numero;
 	}
 	
-	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
+	public Conta buscarNaCollection(int numero) {
+		for (var conta : listaContas) {
+			if (conta.getNumero() == numero) {
+				return conta;
+			}
+		}
 		
-		
+		return null;
 	}
-	
+
 }
