0808f4a359c64b0fc42c3fd954c882be05e276cc
rayane moara <rayane_moara@hotmail.com>
2024-11-12 22:42:43 -0300

    task04

---
+++ src/controller/ContaController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import conta.model.Conta;
+
+
+public class ContaController {
+    
+    public void procurarPorNumero() {
+    	
+    }
+    
+    public void listarTodas() {
+
+	}
+
+	public void cadastrar(Conta conta) {
+
+    }
+
+	public void atualizar(Conta conta) {
+
+	}
+
+    public void deletar(int numero) {
+    	
+    }
+    
+    public void sacar(int numero, float valor) {
+    	
+    }
+
+	public void depositar(int numero, float valor) {
+		
+	}
+	
+	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
+		
+		
+	}
+	
+}
